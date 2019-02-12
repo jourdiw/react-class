8037554ce3c9423f045f54a033dafb731fa1d62f
feychenie <frederik.eychenie@sigfox.com>
2019-02-12 08:11:50 +0100

    feat(shows): searchable shows

--- src/pages/ShowsPage.js
+++ src/pages/ShowsPage.js
@@ -2,20 +2,38 @@ import React, { Component } from "react";
 
 import { List as ShowList } from "../components/Shows";
 
-class BatmanShows extends Component {
-  state = {
-    searching: true,
-    results: []
-  };
+// isolate inital state, so we can use it if we want to reset to it
+const initialState = {
+  searching: false,
+  query: "",
+  results: null
+};
+
+class TvShows extends Component {
+  state = { ...initialState };
 
   // execute the search once mounted on the dom, and 'fetch' API is available
   componentDidMount = () => {
-    this.doSearch();
+    // this.doSearch(); do not execute search on mount anymore
+  };
+
+  handleQueryChange = e => {
+    const queryValue = e.target.value;
+
+    // execute search only if there is at least a letter
+    if (queryValue.length > 0) {
+      this.doSearch(queryValue);
+      this.setState({ query: queryValue });
+    } else {
+      // else reset search
+      this.setState(initialState);
+    }
   };
 
   // query the api using fetch and old school promises.
   doSearch = query => {
-    fetch(`http://api.tvmaze.com/search/shows?q=batman`)
+    this.setState({ searching: true });
+    fetch(`http://api.tvmaze.com/search/shows?q=${query}`)
       .then(res => res.json()) // extracting json from a reponse is another promise
       .then(json =>
         this.setState({
@@ -27,20 +45,27 @@ class BatmanShows extends Component {
   };
 
   render() {
-    const { searching, results } = this.state;
+    const { searching, results, query } = this.state;
 
     // transform [{score, show}] into [show]
-    const shows = results.map(r => r.show);
+    const shows = results ? results.map(r => r.show) : [];
 
     return (
       <div>
-        <h1>Batman shows</h1>
+        <h1>Search Tv Shows</h1>
+        <p>
+          <input
+            type="search"
+            value={query}
+            onChange={this.handleQueryChange}
+          />
+        </p>
         {/** Only renders while searching (initial render) */
         searching && <p>Searching...</p>}
         {/** Renders when search is done AND we have results */
-        !searching && results.length > 0 && (
+        !searching && results && (
           <div>
-            <p>Found at least {results.length} Batman shows</p>
+            <p>Found at least {results.length} Tv shows</p>
             <ShowList shows={shows} />
           </div>
         )}
@@ -49,4 +74,4 @@ class BatmanShows extends Component {
   }
 }
 
-export default BatmanShows;
+export default TvShows;
