958c69ccc9b6f4f63271ced187b469a63a4400a2
feychenie <frederik.eychenie@sigfox.com>
2019-02-12 15:46:01 +0100

    fix(middleware): hot fix

--- src/middlewares/actionLogger.js
+++ src/middlewares/actionLogger.js
@@ -1,6 +1,5 @@
 const actionLogger = store => next => action => {
   console.log("action:", action);
-  next(action);
   let result = next(action);
   console.log("new state", store.getState());
   return result;
